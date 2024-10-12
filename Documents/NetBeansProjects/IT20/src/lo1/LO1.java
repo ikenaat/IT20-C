334e44acb8cd39f2652d9d33325c5ad5e19824b0
ikenaat <kennethgamolo4@gmail.com>
2024-10-11 21:04:17 -0700

    added arraylist

---
+++ Documents/NetBeansProjects/IT20/src/lo1/LO1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lo1;
+
+/**
+ *
+ * @author HP
+ */
+public class LO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Array 
+        String[] names = {"kenneth", "ken", "neth"};
+        
+        //Output to print the values of the Array
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        
+        System.out.println("~~~~~~~");
+        
+        //Process to print all the values in the array using for Loops
+        for(int i = 0; i < names.length; i++) {
+            System.out.print(names[i] + ", ");
+        }
+    }
+    
+}
